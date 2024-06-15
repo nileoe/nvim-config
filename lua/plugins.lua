bef03934ba1a92d2d3f3989eeea7fb1bd6628296
nileoe <linoperdrix@gmail.com>
2024-06-15 16:26:17 +0100

    changed flash select ts keybind

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -514,7 +514,7 @@ dependencies = {
     -- stylua: ignore
     keys = {
       { "<leader>/", mode = { "n", "x", "o" }, function() require("flash").jump() end,       desc = "Flash" },
-      { "<leader>?", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "<leader>v", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
       -- { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
       -- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
       -- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
